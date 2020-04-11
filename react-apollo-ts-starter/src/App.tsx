6fd8afa97dfc3272c57aaaa3f7d6930b0fbcd2c9
Myeongwhan Cho <MyeonghCho@gmail.com>
2020-04-11 16:53:30 +0900

    Add global style with reset css

--- react-apollo-ts-starter/src/App.tsx
+++ react-apollo-ts-starter/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Home} from './routes';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
+      <GlobalStyle />
+    </>
   );
 };
 
