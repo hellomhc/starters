36f9ab1c10ce682afd3706edcdf61a497af93790
Myeongwhan Cho <MyeonghCho@gmail.com>
2020-04-11 16:48:22 +0900

    Add react-router-dom

--- react-apollo-ts-starter/src/App.tsx
+++ react-apollo-ts-starter/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Home} from './routes';
 
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
