4bfba1ce9b0076586d88a040429a94975737e668
Myeongwhan Cho <MyeonghCho@gmail.com>
2020-04-11 15:38:40 +0900

    Add dotenv

--- apollo-express-ts-starter/src/index.ts
+++ apollo-express-ts-starter/src/index.ts
@@ -1,21 +1,23 @@
 import express from 'express';
 import http from 'http';
-import server from './apollo'
-
-const PORT = 3000;
+import server from './apollo';
 
 const app = express();
 
 // use middleware
-server.applyMiddleware({ app, path: '/graphql' });
+server.applyMiddleware({app, path: '/graphql'});
 
 // create HTTP server
 const httpServer = http.createServer(app);
 
-// add subscriptions 
+// add subscriptions
 server.installSubscriptionHandlers(httpServer);
 
-httpServer.listen(({ port: PORT }), (() => {
-  console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
-  console.log(`Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`)
-}));
+httpServer.listen({port: process.env.SERVER_PORT}, () => {
+  console.log(
+    `Server ready at http://localhost:${process.env.SERVER_PORT}${server.graphqlPath}`,
+  );
+  console.log(
+    `Subscriptions ready at ws://localhost:${process.env.SERVER_PORT}${server.subscriptionsPath}`,
+  );
+});
