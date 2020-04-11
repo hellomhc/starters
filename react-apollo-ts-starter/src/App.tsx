e745fb390d07a847c393413983176102e933d97c
Myeongwhan Cho <MyeonghCho@gmail.com>
2020-04-11 16:58:01 +0900

    Add typescript definitions for styled-components

--- react-apollo-ts-starter/src/App.tsx
+++ react-apollo-ts-starter/src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {ThemeProvider} from 'styled-components';
 import {Home} from './routes';
-import GlobalStyle from './styles/global';
+import {global as GlobalStyle, theme} from './styles';
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router>
         <Switch>
           <Route exact path='/' component={Home} />
         </Switch>
       </Router>
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ react-apollo-ts-starter/src/styles/theme.ts
@@ -0,0 +1,10 @@
+import {DefaultTheme} from 'styled-components';
+
+const theme: DefaultTheme = {
+  color: {
+    white: '#fff',
+    black: '#fff',
+  },
+};
+
+export default theme;
